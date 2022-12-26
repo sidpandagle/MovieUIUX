4655b2371483b77b1def84945613c6ae913e93d5
sidpandagle <siddhant.pandagle1998@gmail.com>
2022-12-26 23:32:21 +0530

    Page change functionality improved

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,30 +8,69 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'movie-showcase';
   bkwallpapers: Background[];
+  currentBackgroundIndex: number;
   constructor() {
-    this.bkwallpapers = [
-      {
-        imgUrl: '../assets/movie-pics/lalaland.jpg',
-        selected: true
-      },
-      {
-        imgUrl: '../assets/movie-pics/wonderwoman.jpg',
-        selected: false
-      }
+    this.currentBackgroundIndex=3;
+    this.bkwallpapers = [{
+      imgUrl: '../assets/movie-pics/captainamerica.jpg',
+      selected: false
+    }, {
+      imgUrl: '../assets/movie-pics/inception.jpg',
+      selected: false
+    }, {
+      imgUrl: '../assets/movie-pics/kingkong.jpg',
+      selected: false
+    }, {
+      imgUrl: '../assets/movie-pics/lalaland.jpg',
+      selected: true
+    }, {
+      imgUrl: '../assets/movie-pics/starwars.jpg',
+      selected: false
+    },
+    {
+      imgUrl: '../assets/movie-pics/tron.jpg',
+      selected: false
+    }, {
+      imgUrl: '../assets/movie-pics/wonderwoman.jpg',
+      selected: false
+    }
     ]
   }
 
-  changeBackground(index:number){
-    this.bkwallpapers.forEach(res=>{
-      res.selected=false;
+  changeBackground(index: number) {
+    this.currentBackgroundIndex = index;
+    this.bkwallpapers.forEach(res => {
+      res.selected = false;
     })
-    this.bkwallpapers[index].selected=true;
+    this.bkwallpapers[this.currentBackgroundIndex].selected = true;
   }
 
-  selectedBackground(){
-    return this.bkwallpapers.filter(res=>res.selected)[0].imgUrl;
+  nextBackground() {
+    if(this.currentBackgroundIndex==this.bkwallpapers.length-1){
+      this.currentBackgroundIndex = 0
+    }else{
+      this.currentBackgroundIndex++;
+    }
+    this.bkwallpapers.forEach(res => {
+      res.selected = false;
+    })
+    this.bkwallpapers[this.currentBackgroundIndex].selected = true;
+  }
+  prevBackground() {
+    if(this.currentBackgroundIndex==0){
+      this.currentBackgroundIndex = this.bkwallpapers.length-1
+    }else{
+      this.currentBackgroundIndex--;
+    }
+    this.bkwallpapers.forEach(res => {
+      res.selected = false;
+    })
+    this.bkwallpapers[this.currentBackgroundIndex].selected = true;
   }
 
+  selectedBackground() {
+    return this.bkwallpapers.filter(res => res.selected)[0].imgUrl;
+  }
 
 }
 
